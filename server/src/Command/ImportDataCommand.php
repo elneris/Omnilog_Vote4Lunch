a73121732f426d5bca11869778a64da2da72a9f7
Elneris <elneris.dang@omnilog.fr>
2019-09-23 13:39:16 +0200

    ajout d'une commande symfony afin de rajouter les places en bdd

--- server/src/Command/ImportDataCommand.php
+++ server/src/Command/ImportDataCommand.php
@@ -3,7 +3,7 @@
 namespace App\Command;
 
 use App\Entity\Place;
-use Doctrine\ORM\EntityManager;
+use App\Services\ImportData;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -13,14 +13,15 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 
 class ImportDataCommand extends Command
 {
-    private $entityManager;
+    protected $importData;
 
     protected static $defaultName = 'app:import-data';
 
-    public function __construct(string $name = null, EntityManager $entityManager)
+    public function __construct(string $name = null, ImportData $importData)
     {
         parent::__construct($name);
-        $this->entityManager = $entityManager;
+
+        $this->importData = $importData;
     }
 
     protected function configure()
@@ -33,7 +34,9 @@ class ImportDataCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $jsonPlaces = file_get_contents('./json_data/places.json');
-        $places = json_decode($jsonPlaces);
+        $places = json_decode($jsonPlaces, true);
+
+        $this->importData->deleteData();
 
         foreach ($places as $place) {
 
@@ -48,55 +51,110 @@ class ImportDataCommand extends Command
             $website = null;
             $openingHours = null;
             $cuisine = null;
-            $regPhone = '/^(?:(?:\+ | 00)33 | 0)\s * ([1 - 9])(?:[\s . -] * (\d{2}))(?:[\s . -] * (\d{2}))(?:[\s . -] * (\d{2}))(?:[\s . -] * (\d{2}))$/';
+            $regPhone = '/^(?:(?:\+|00)33|0)\s*([1-9])(?:[\s.-]*(\d{2}))(?:[\s.-]*(\d{2}))(?:[\s.-]*(\d{2}))(?:[\s.-]*(\d{2}))$/';
 
-            $addrStreet = null;
-            $addrNumber = null;
-            $contactStreet = null;
-            $contactNumber = null;
 
-            if (isset($place->tags->{'addr:housenumber'})){
-                $addrNumber = $place->tags->{'addr:housenumber'};
+            // Import Address
+            if ((isset($place['tags']['addr:housenumber']) && isset($place['tags']['addr:street']))) {
+                $address = $place['tags']['addr:housenumber'] . ' ' . $place['tags']['addr:street'];
+            } elseif ((isset($place['tags']['contact:housenumber']) && isset($place['tags']['contact:street']))) {
+                $address = $place['tags']['contact:housenumber'] . ' ' . $place['tags']['contact:street'];
+            } elseif (isset($place['tags']['addr:street'])) {
+                $address = $place['tags']['addr:street'];
+            } elseif (isset($place['tags']['contact:street'])) {
+                $address = $place['tags']['contact:street'];
             }
 
-            if (isset($place->tags->{'addr:street'})){
-                $addrStreet = $place->tags->{'addr:street'};
+
+            // Import City
+            if (isset($place['tags']['addr:city'])) {
+                $city = $place['tags']['addr:city'];
+            } elseif (isset($place['tags']['contact:city'])) {
+                $city = $place['tags']['contact:city'];
             }
 
-            if (isset($place->tags->{'contact:housenumber'})){
-                $contactNumber = $place->tags->{'contact:housenumber'};
-            }
 
-            if (isset($place->tags->{'contact:street'})){
-                $contactStreet = $place->tags->{'contact:street'};
-            }
-
-            if ($addrNumber && $addrStreet) {
-                $address = $addrNumber . ' ' . $addrStreet;
-            } elseif ($contactNumber && $contactStreet) {
-                $address = $contactNumber . ' ' . $contactStreet;
-            } elseif ($addrStreet) {
-                $address = $addrStreet;
-            } elseif ($contactStreet) {
-                $address = $contactStreet;
-            }
-
-            dd($input);
-
-
-            if ((isset($place->tags->{'addr:city'}) || isset($place->tags->{'contact:city'}))) {
-                $city = $place->tags->{'addr:city'} || $place->tags->{'contact:city'};
-            }
-
-            if ($place['tags']['phone']) {
-                $phoneArray = $place['tags']['phone'] . match(reg);
-
+            // Import Phone Number
+            if (isset($place['tags']['phone'])) {
+                preg_match($regPhone, $place['tags']['phone'], $phoneArray);
                 $phone = 0 . $phoneArray[1] . $phoneArray[2] . $phoneArray[3] . $phoneArray[4] . $phoneArray[5];
-            } else if ($place['tags']['contact:phone']) {
-                $phoneArray = $place['tags']['contact:phone'] . match(reg);
-
+            } else if (isset($place['tags']['contact:phone'])) {
+                preg_match($regPhone, $place['tags']['contact:phone'], $phoneArray);
                 $phone = 0 . $phoneArray[1] . $phoneArray[2] . $phoneArray[3] . $phoneArray[4] . $phoneArray[5];
             }
+
+
+            // Import Email
+            if (isset($place['tags']['email'])) {
+                $email = $place['tags']['email'];
+            } elseif (isset($place['tags']['contact:email'])) {
+                $email = $place['tags']['contact:email'];
+            }
+
+
+            // Import Website
+            if (isset($place['tags']['website'])) {
+                $website = $place['tags']['website'];
+            } elseif (isset($place['tags']['contact:website'])) {
+                $website = $place['tags']['contact:website'];
+            } elseif (isset($place['tags']['brand:website'])) {
+                $website = $place['tags']['brand:website'];
+            }
+
+
+            // Import Cuisines
+            if (isset($place['tags']['cuisine'])) {
+                //$cuisine = explode(';', $place['tags']['cuisine']);
+                $cuisine = str_replace(';', ', ', $place['tags']['cuisine'] );
+            }
+
+
+            // Import Opening Hours
+            if (isset($place['tags']['opening_hours'])) {
+                $days = [
+                    'Mo' => 'Lu',
+                    'Tu' => 'Ma',
+                    'We' => 'Me',
+                    'Th' => 'Je',
+                    'Fr' => 'Ve',
+                    'Sa' => 'Sa',
+                    'Su' => 'Di',
+                ];
+
+                preg_match_all('/Mo|Tu|We|Th|Fr|Sa|Su/i', $place['tags']['opening_hours'], $matches );
+
+                $openingHours = $place['tags']['opening_hours'];
+                foreach ($matches[0] as $match) {
+                    $openingHours = str_replace($match, $days[ucfirst($match)], $openingHours);
+                }
+            }
+
+            $test = [
+                $address,
+                $city,
+                $phone,
+                $email,
+                $website,
+                $openingHours,
+                $cuisine,
+            ];
+
+            $restaurant->setName($place['tags']['name']);
+            $restaurant->setLat($place['lat']);
+            $restaurant->setLng($place['lon']);
+            $restaurant->setType($place['tags']['amenity']);
+            $restaurant->setAddress($address);
+            $restaurant->setCity($city);
+            $restaurant->setPhone($phone);
+            $restaurant->setEmail($email);
+            $restaurant->setWebsite($website);
+            $restaurant->setOpeningHours($openingHours);
+            $restaurant->setCuisine($cuisine);
+            $restaurant->setCreatedAt(new \DateTime());
+            $restaurant->setUpdatedAt(new \DateTime());
+
+            $this->importData->importData($restaurant);
         }
+        $output->writeln("Place successfully added to the database.");
     }
 }
