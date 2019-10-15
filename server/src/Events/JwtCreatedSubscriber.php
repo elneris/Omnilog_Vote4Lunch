aab3912b2f934b111f9a5278a9ed8c9dd310de7d
Elneris <elneris.dang@omnilog.fr>
2019-10-15 10:11:00 +0200

    change expiration token +1day

--- server/src/Events/JwtCreatedSubscriber.php
+++ server/src/Events/JwtCreatedSubscriber.php
@@ -18,6 +18,13 @@ class JwtCreatedSubscriber
         $data['pseudo'] = $user->getPseudo();
         $data['email'] = $user->getEmail();
 
+        $expiration = new \DateTime('+1 day');
+        $expiration->setTime(2, 0, 0);
+
+        $payload = $event->getData();
+        $payload['exp'] = $expiration->getTimestamp();
+
         $event->setData($data);
+        $event->setData($payload);
     }
 }
